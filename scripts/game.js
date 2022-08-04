38cdfc8422d776fd33b4963ac7effe2b9bc04877
skrutosha00 <istov00@bk.ru>
2022-08-04 23:54:12 +0300

    2

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,5 @@
 import { changeBalance, randInt, setBalanceField } from "./functions.js"
 
-if (!localStorage.getItem('chosen_back')) {
-    localStorage.setItem('chosen_back', '1')
-}
-
-if (!localStorage.getItem('balance_back')) {
-    localStorage.setItem('balance_back', 5000)
-}
-
 setBalanceField()
 
 let active = 'roll'
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+if (!localStorage.getItem('chosen_back')) {
+    localStorage.setItem('chosen_back', '1')
+}
+
+if (!localStorage.getItem('balance_back')) {
+    localStorage.setItem('balance_back', 5000)
+}
